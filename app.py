7f96231e04d6ba5195ccc3aa90559bc68f094e73
DESKTOP-G3USBLG\simba <simba0502@naver.com>
2022-05-06 20:04:41 +0900

    제출합니다

--- app.py
+++ app.py
@@ -13,44 +13,62 @@ db = client.test
 def index():
     return render_template('index.html')
 
-@app.route('/detail/<idx>', methods=['DELETE'])
+@app.route('/detail/<idx>')
 def detail(idx):
-    db.test.delete_one({"idx": idx})
-    return {"result":"success"}
+    article = db.articles.find_one({'idx': int(idx)}, {'_id': False})
+    return render_template('detail.html',article=article)
 
-@app.route('/post', methods=['POST'])
-def save_post():
-    title = request.form['title']
-    pw = request.form['pw']
-    comment = request.form['comment']
-
-    maxIdx = db.post.find().sort('idx',-1)[0]['idx'];
-    print(maxIdx);
-    post = {
-        'idx':maxIdx+1,
-        'title': title,
-        'content':comment,
+# Create
+@app.route('/article', methods=['POST'])
+def create_article():
+    title = request.form['title'];
+    content = request.form['content'];
+    pw = request.form['pw'];
+    count = db.articles.estimated_document_count({})
+    if count == 0:
+        count=1
+    else:
+        count = (list(db.articles.find({}).sort([("id", -1)])))[0]['id'] + 1
+    article = {
+        'id':count,
+        'title':title,
+        'content':content,
         'pw':pw,
-        'reg_date': datetime.datetime.now()
+        'count':0,
+        'create_date': datetime.now()
     }
-
-    db.test.insert_one(post)
-    return jsonify({'result': 'success', 'msg': f'example "{title}" saved'})
-
-
-@app.route('/post', methods=['GET'])
-def get_post():
-    posts = 0
-
-    return jsonify({"posts": posts})
-
-
-@app.route('/post', methods=['DELETE'])
-def delete_post():
-    idx = request.args.get('idx')
-    db.test.delete_one({'idx': int(idx)})
+    db.articles.insert_one(article)
     return {"result": "success"}
 
+# Read
+@app.route('/article', methods=['GET'])
+def read_article():
+    order = request.args.get('order')
+    if order == 'asc':
+        articles = list(db.articles.find({}, {'_id': False}).sort([("create_date", 1)]))
+    else:
+        articles = list(db.articles.find({}, {'_id': False}).sort([("create_date", -1)]))
+    for article in articles:
+        article['create_date'] = article['create_date'].strftime('%Y.%m.%d %H:%M:%S')
+    return jsonify({"articles": articles})
+
+# Update
+@app.route('/article', methods=['PUT'])
+def update_article():
+
+    return {"result": "success"}
+
+@app.route('/article/count/<id>', methods=['PUT'])
+def update_article_count(id):
+    count = db.articles.find_one({'id': int(id)}, {'_id': False})['count']
+    db.articles.update_one({'id': id},{ "$set": { "count": int(count)+1 } })
+    return {"result": "success"}
+
+# Delete
+@app.route('/article', methods=['DELETE'])
+def delete_article():
+
+    return {"result": "success"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=True)
