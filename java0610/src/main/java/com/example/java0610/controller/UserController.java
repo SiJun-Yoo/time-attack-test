4181040983a2cc2c2f7934d091e8898b15c65df7
DESKTOP-G3USBLG\simba <simba0502@naver.com>
2022-06-10 20:08:17 +0900

    @Controller -> @RestController

--- java0610/src/main/java/com/example/java0610/controller/UserController.java
+++ java0610/src/main/java/com/example/java0610/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class UserController {
     private final UserService userService;
 
